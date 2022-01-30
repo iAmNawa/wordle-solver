bccf9f16f49d776cc730e440471fc99248a5e79e
Paul Borawski <paulborawski14@gmail.com>
2022-01-30 08:49:35 -0800

    remove console logs

--- solver.js
+++ solver.js
@@ -1,5 +1,5 @@
-let correct_word = 'hinaz'
-let start_word ='sotuz'
+let correct_word = 'spite'
+let start_word ='scram'
 let found = false
 const data = require("./words.json")
 
@@ -7,7 +7,7 @@ let correctArr = [0,0,0,0,0]
 
 for (var i = 0; i < correctArr.length; i++) {
   if (correct_word[i] === start_word[i]) {
-    correctArr[i] = 1
+    correctArr[i] = start_word[i]
   }
 }
 
@@ -16,7 +16,7 @@ console.log(correctArr)
 let possibleWords = []
 
 for (var i = 0; i < correctArr.length; i++) {
-  if (correctArr[i] === 1) {
+  if (correctArr[i] == true) {
     for (var j = 0; j < data.length; j++) {
       if (data[j][i] === correct_word[i]) {
         possibleWords.push(data[j])
@@ -29,11 +29,3 @@ let unique = [...new Set(possibleWords)];
 
 console.log(unique)
 console.log(unique.length)
-
-/*for (var i = 0; i < data.length; i ++) {
-    if (data[i] === word) {
-        found = true
-    }
-}*/
-
-//console.log(found)
